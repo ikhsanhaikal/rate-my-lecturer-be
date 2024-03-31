ffeba07fe3978c889e123ea5eda4b6189a0e0b6a
Ikhsan Haikal <47350060+ikhsanhaikal@users.noreply.github.com>
2024-03-31 23:21:00 +0700

    added prisma module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,35 +3,15 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 import { LecturersModule } from './lecturers/lecturers.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { LabsModule } from './labs/labs.module';
-import { Lecturer } from './lecturers/lecturers.entity';
-import { Lab } from './labs/labs.entity';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { CoursesModule } from './courses/courses.module';
 import { SubjectModule } from './subjects/subjects.module';
-import { Subject } from './subjects/subjects.entity';
-import { Course } from './courses/courses.entity';
+import { LabsModule } from './labs/labs.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({}),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => {
-        return {
-          type: 'mysql',
-          host: configService.get('DB_HOST'),
-          port: configService.get<number>('DB_PORT'),
-          username: configService.get('DB_USER'),
-          password: configService.get('DB_PASS'),
-          database: configService.get('DB_DATABASE'),
-          entities: [Lecturer, Lab, Subject, Course],
-          synchronize: false,
-        };
-      },
-      inject: [ConfigService],
-    }),
     LabsModule,
     LecturersModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
@@ -43,6 +23,7 @@ import { Course } from './courses/courses.entity';
     }),
     CoursesModule,
     SubjectModule,
+    PrismaModule,
   ],
 })
 export class AppModule {}
