984747645a2ec1c1ae1df2830baf0a8689cc0cbf
Ikhsan Haikal <47350060+ikhsanhaikal@users.noreply.github.com>
2024-05-09 14:36:57 +0700

    added seed fo reviews

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,13 +1,15 @@
-import { Gender, Lab, LabCode, PrismaClient, Subject } from '@prisma/client';
+import { Gender, Lab, User, PrismaClient, Subject } from '@prisma/client';
 import { faker } from '@faker-js/faker';
 import { labs, subjects, traits } from './dummy-data';
 const prisma = new PrismaClient();
 
 async function main() {
-  await seedUsers();
+  const users = await seedUsers();
   await seedTraits();
   await seedLecturers();
+  seedReviews(users);
 }
+
 main()
   .then(async () => {
     await prisma.$disconnect();
@@ -17,6 +19,7 @@ main()
     await prisma.$disconnect();
     process.exit(1);
   });
+
 async function seedTraits() {
   const foo = traits.map(async (trait) => {
     return prisma.trait.upsert({
@@ -29,25 +32,35 @@ async function seedTraits() {
   });
   return await Promise.all(foo);
 }
-function seedSubjects() {
+function seedSubjects(editor: any) {
   return subjects.map(async (subject) => {
     return prisma.subject.upsert({
       where: { name: subject },
       update: {},
       create: {
         name: subject,
+        editor: {
+          connect: {
+            id: editor.id,
+          },
+        },
       },
     });
   });
 }
-function seedLabs() {
+function seedLabs(editor: any) {
   return labs.map(async (lab) => {
     return prisma.lab.upsert({
-      where: { code: lab.code as LabCode },
+      where: { code: lab.code },
       update: {},
       create: {
         name: lab.name,
-        code: lab.code as LabCode,
+        code: lab.code,
+        editor: {
+          connect: {
+            id: editor.id,
+          },
+        },
       },
     });
   });
@@ -66,15 +79,28 @@ async function seedUsers() {
       update: {},
       create: {
         email: email,
-        username: `${firstName}${lastName}`,
+        username: `${firstName} ${lastName}`,
       },
     });
   });
-  await Promise.all(req);
+  const users = await Promise.all(req);
+  return users;
 }
+
 async function seedLecturers() {
-  const resultLabs: Array<Lab> = await Promise.all(seedLabs());
-  const resultSubjects: Array<Subject> = await Promise.all(seedSubjects());
+  const editor = await prisma.editor.upsert({
+    where: { email: 'ikhsanhoehoe@gmail.com' },
+    update: {},
+    create: {
+      email: 'ikhsanhoehoe@gmail.com',
+      username: 'ikhsan haikal',
+    },
+  });
+  const resultLabs: Array<Lab> = await Promise.all(seedLabs(editor));
+  const resultSubjects: Array<Subject> = await Promise.all(
+    seedSubjects(editor),
+  );
+
   const req = new Array(30).fill(0).map(async () => {
     const sex = faker.person.sex();
     const firstName = faker.person.firstName(sex as 'female' | 'male');
@@ -91,6 +117,11 @@ async function seedLecturers() {
         name: `${firstName} ${lastName}`,
         gender: sex.toUpperCase() as Gender,
         description: faker.lorem.sentence(),
+        editor: {
+          connect: {
+            id: editor.id,
+          },
+        },
         lab: {
           connect: resultLabs[faker.number.int({ max: resultLabs.length - 1 })],
         },
@@ -103,6 +134,12 @@ async function seedLecturers() {
                   resultSubjects[
                     faker.number.int({ max: resultSubjects.length - 1 })
                   ].id,
+                semester: 'GENAP',
+                year: faker.date.between({
+                  from: '2015-01-01T00:00:00.000Z',
+                  to: '2024-01-01T00:00:00.000Z',
+                }),
+                editorId: editor.id,
               };
             }),
           },
@@ -111,4 +148,36 @@ async function seedLecturers() {
     });
   });
   await Promise.all(req);
+}
+
+async function seedReviews(users: User[]) {
+  const courses = await prisma.class.findMany({});
+
+  const req1 = courses.map(async (course) => {
+    return prisma.review.createMany({
+      skipDuplicates: true,
+      data: new Array(faker.number.int(5)).fill(0).map(() => ({
+        comment: faker.lorem.lines(),
+        rating: faker.number.float() * 5,
+        classId: course.id,
+        reviewerId: users[faker.number.int(users.length - 1)].id,
+      })),
+    });
+  });
+
+  await Promise.all(req1);
+  const traits = await prisma.trait.findMany({});
+  const reviews = await prisma.review.findMany({});
+
+  const req2 = reviews.map(async (review) => {
+    return prisma.tag.createMany({
+      skipDuplicates: true,
+      data: new Array(faker.number.int(3)).fill(0).map(() => ({
+        traitId: traits.at(faker.number.int(traits.length - 1)).id,
+        reviewId: review.id,
+      })),
+    });
+  });
+
+  await Promise.all(req2);
 }
